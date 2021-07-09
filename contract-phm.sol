fe90f6f5a419739252a1490679e9446cfd5d0bd2
AndyatFocallocal <focallocal.london@gmail.com>
2021-07-10 00:22:33 +0100

    Files created via Git Gist, Remix and Openzepplin

---
+++ contract-phm.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts@4.2.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.2.0/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.2.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.2.0/access/Ownable.sol";
+
+contract PublicHappiness is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("Public Happiness", "PHAT") {
+        _mint(msg.sender, 7900000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
